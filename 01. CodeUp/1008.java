4d5b0544e3adc608a35f599e95f184a2cb07152b
dev-wanted <chaa822@naver.com>
2022-05-12 10:54:01 +0900

    1008.java

---
+++ 01. CodeUp/1008.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws Exception {
+        System.out.printf("\u250C\u252C\u2510\n");
+        System.out.printf("\u251C\u253C\u2524\n");
+        System.out.printf("\u2514\u2534\u2518\n");
+    }
+}
