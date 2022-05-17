0e43e806043e53f99b5267e69e36d45f1fc14514
dev-wanted <chaa822@naver.com>
2022-05-17 10:19:36 +0900

    1012.java

---
+++ 01. CodeUp/1012.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    
+    public static final Scanner scanner = new Scanner(System.in);
+    public static final BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+
+	public static void main(String[] args) throws Exception {
+        float f = scanner.nextFloat();
+        System.out.printf("%f", f);
+    }
+}
