b085f957707e4728f90f04996473fb9f0b364d53
jaeoh <chaa822@naver.com>
2022-06-05 23:24:45 +0900

    1029.java

---
+++ 01. CodeUp/1029.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class Main {
+    
+    public static final Scanner scanner = new Scanner(System.in);
+
+	public static void main(String[] args) throws Exception {
+        double d = scanner.nextDouble();
+        System.out.printf("%.11f", d);
+    }
+}
