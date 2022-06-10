a150d8793bff887be07ebe53ddece6e407cde3d9
jaeoh <chaa822@naver.com>
2022-06-11 00:37:23 +0900

    1031.java

---
+++ 01. CodeUp/1031.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class Main {
+    
+    public static final Scanner scanner = new Scanner(System.in);
+
+	public static void main(String[] args) throws Exception {
+        int n = scanner.nextInt();
+        System.out.printf( "%o", n);
+    }
+}
