86677e322318ca7d8224ecee98bbde9507bf5649
jaeoh <chaa822@naver.com>
2022-06-11 00:36:15 +0900

    1030.java

---
+++ 01. CodeUp/1030.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class Main {
+    
+    public static final Scanner scanner = new Scanner(System.in);
+
+	public static void main(String[] args) throws Exception {
+        long n = scanner.nextLong();
+        System.out.println(n);
+    }
+}
