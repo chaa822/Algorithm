44de39092d764ecdc5a9da064762e85e09fd9643
jaeoh <chaa822@naver.com>
2022-06-23 10:33:01 +0900

    1040.java

---
+++ 01. CodeUp/1040.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    
+    public static final Scanner scanner = new Scanner(System.in);
+    public static final BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+
+	public static void main(String[] args) throws Exception {
+        int a = scanner.nextInt();
+        System.out.println( -a );
+    }
+}
