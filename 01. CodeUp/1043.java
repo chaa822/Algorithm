325d2051e6cde581b39b16862b1d3f349ebab1ef
jaeoh <chaa822@naver.com>
2022-07-02 23:28:49 +0900

    1043.java

---
+++ 01. CodeUp/1043.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    
+    public static final Scanner scanner = new Scanner(System.in);
+    public static final BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+
+	public static void main(String[] args) throws Exception {
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        System.out.println( a % b);
+    }
+}
