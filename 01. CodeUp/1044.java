38842bac932039104919f0c872dfed5944cd0247
jaeoh <chaa822@naver.com>
2022-07-07 15:32:23 +0900

    1044.java

---
+++ 01. CodeUp/1044.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    
+    public static final Scanner scanner = new Scanner(System.in);
+    public static final BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+
+	public static void main(String[] args) throws Exception {
+        long n = scanner.nextLong();
+        System.out.println( n + 1 );
+    }
+}
