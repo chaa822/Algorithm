04eff46a7dbcec985404973c9f774e6e6709ec94
jaeoh <chaa822@naver.com>
2022-07-28 10:18:01 +0900

    1047.java

---
+++ 01. CodeUp/1047.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+	
+	public static final Scanner scanner = new Scanner(System.in);
+	
+	public static void main(String[] args) throws Exception {
+	    int a = scanner.nextInt();
+	    System.out.println( a * 2 );
+	}
+}
